0114e518990c8706070153511abbbb0c406b62e7
vasili kisel <eugene.dombrovski@gmail.com>
2019-04-19 14:00:11 +0300

    check: auto deploy version 1.2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        juice-punch one more check auto deploy version 1.1
+        juice-punch one more check auto deploy version 1.2
       </div>
     );
   }
