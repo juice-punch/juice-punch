659527be25e8e526fa2046c1249c9d94f91b709b
vasili kisel <eugene.dombrovski@gmail.com>
2019-04-23 18:12:27 +0300

    master test for app review

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
-        juice-punch check pipeline
+        juice-punch check pipeline pr master test
       </div>
     );
   }
