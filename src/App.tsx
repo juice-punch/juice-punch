956ce5f995f77469fc7fd39789c6db84ddbb85da
vasili kisel <eugene.dombrovski@gmail.com>
2019-04-24 13:07:20 +0300

    check new app review

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
-        juice-punch enable waiting for CI
+        juice-punch enable waiting for CI check develop app review
       </div>
     );
   }
