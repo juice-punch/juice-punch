dc7b07b397b9659e624be15a3a1e2d4835a3ac36
vasili kisel <eugene.dombrovski@gmail.com>
2019-04-24 12:27:07 +0300

    update develop

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
-        juice-punch new heroku repo check autodeploy master branch test new
+        juice-punch update develop
       </div>
     );
   }
